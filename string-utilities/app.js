ad19734d0300a8164c6731620b8dc7be6ab2914a
Shanmugapriya <priyajanarthanan@gmail.com>
2025-04-28 08:43:49 -0500

    Added output

--- string-utilities/app.js
+++ string-utilities/app.js
@@ -2,6 +2,6 @@
 const stringUtils = require('./stringUtils');
 
 
-console.log(stringUtils.reverse("1232"));
-console.log(stringUtils.contains("Good morning","morning"));
-console.log(stringUtils.capitalize("ggdd"));
+console.log(stringUtils.reverse("1232")); // Output: Invalid Input!
+console.log(stringUtils.contains("Good morning","morning")); // output : true
+console.log(stringUtils.capitalize("ggdd")); // Output: Ggdd
